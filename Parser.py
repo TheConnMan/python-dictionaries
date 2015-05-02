9ec1ab41a06b9ec7f5cd4d4a6ab55cf3be544421
TheConnMan <bcconn2112@gmail.com>
2015-05-02 12:17:00 -0400

    Added comments and minor parseLSA changes.

--- Parser.py
+++ Parser.py
@@ -3,19 +3,19 @@ import csv
 def parseLSA():
     header = [];
     LSADict = {};
-    with open('data/LSAsamples.csv', 'rb') as csvfile:
+    with open('data/LSAsamples.csv', 'rb') as csvfile:                  # Open and read the CSV line by line
         LSAreader = csv.reader(csvfile, delimiter=',');
         for row in LSAreader:
-            if len(header) == 0:
-                header = [cell.lower() for cell in row];
+            if len(header) == 0:                                        # If the header hasn't been initialized, initialize it
+                header = [cell.lower() for cell in row];                # Turn each header element into lowercase
                 for cell in header:
-                    if cell != '':
-                        LSADict[cell] = {};
+                    if cell != '':                                      # If a cell isn't blank add it as a key to the dict
+                        LSADict[cell] = {};                             # Result: LSADict = {'street': {}, 'taxi': {}, 'tongue': {}}
             else:
-                for i in range(0, len(header)):
-                    if header[i] != '' and row[i + 1] != '':
-                        LSADict[header[i]][row[i]] = float(row[i + 1]);
-                
+                for i in range(0, len(LSADict.keys())):                 # Start adding values to dict
+                    if row[3 * i + 1] != '':                            # If value exists (because some columns are shorter than others)
+                        score = float(row[3 * i + 1]);                  # Convert the score to a float
+                        LSADict[header[3 * i]][row[3 * i]] = score;     # Add the score to the dict (e.g. LSADict['street']['bike'] = 0.3)
     return LSADict
 
 def parseResponses(fileName):
@@ -24,31 +24,31 @@ def parseResponses(fileName):
     with open('data/' + fileName, 'rb') as csvfile:
         responseReader = csv.reader(csvfile, delimiter=',');
         for row in responseReader:
-            if len(header) == 0:
-                header = row;
+            if len(header) == 0:                                        # If the header hasn't been initialized, initialize it
+                header = row;                                           # Just set the header to be the header row
             else:
-                response = getResponseObject(header, row);
-                responseList.append(response);
+                response = getResponseObject(header, row);              # Get the response object
+                responseList.append(response);                          # Add it to the response list
     return responseList;
 
 def getResponseObject(header, row):
-    response = {};
-    for i in range(0, len(row)):
-        response[header[i]] = row[i];
+    response = {};                                                      # Initialize the response dict
+    for i in range(0, len(row)):                                        # For each column
+        response[header[i]] = row[i];                                   # Add the value to the response dict under the header key
     return response;
 
 def calculateScore(LSADict, response):
     LSAKeys = LSADict.keys()
     score = 0;
-    for key in LSAKeys:
-        if response[key] in LSADict[key]:
-            score += LSADict[key][response[key]];
-        else:
+    for key in LSAKeys:                                                 # For each key in the LSA dict
+        if response[key] in LSADict[key]:                               # Check to make sure the reponse value has a score associated with it
+            score += LSADict[key][response[key]];                       # Add the response score to the response's total score
+        else:                                                           # Print an error if a response doesn't have a score
             print 'ERROR: Response value=' + response[key] + ' doesn\'t exist.'
     return score;
 
-LSADict = parseLSA();
-responses = parseResponses('responsefilesample.csv');
+LSADict = parseLSA();                                                   # Get the dict
+responses = parseResponses('responsefilesample.csv');                   # Parse the responses
 for response in responses:
-    score = calculateScore(LSADict, response);
+    score = calculateScore(LSADict, response);                          # Calculate and print each response score
     print 'Score for subject *' + response['subject'] + '* is ' + str(score)
