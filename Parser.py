cb89234be555b294db4221d817aeb3d07afcdeae
TheConnMan <bcconn2112@gmail.com>
2015-05-02 11:48:08 -0400

    Added parseResponse function.

--- Parser.py
+++ Parser.py
@@ -18,5 +18,26 @@ def parseLSA():
                 
     return LSADict
 
+def parseResponses(fileName):
+    header = [];
+    responseList = [];
+    with open('data/' + fileName, 'rb') as csvfile:
+        responseReader = csv.reader(csvfile, delimiter=',');
+        for row in responseReader:
+            if len(header) == 0:
+                header = row;
+            else:
+                response = getResponseObject(header, row);
+                responseList.append(response);
+    return responseList;
+
+def getResponseObject(header, row):
+    response = {};
+    for i in range(0, len(row)):
+        response[header[i]] = row[i];
+    return response;
+
 LSADict = parseLSA();
+responses = parseResponses('responsefilesample.csv');
 print LSADict
+print responses
